b299eef257f9f5e178a52ecf7277d641f3085177
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2021-11-24 16:57:29 +0000

    Update comment.

--- solutions/Z3.Linq.Examples/RecordTheorem.cs
+++ solutions/Z3.Linq.Examples/RecordTheorem.cs
@@ -1,10 +1,10 @@
 ﻿namespace Z3.Linq.Examples;
 
-// This doesn't work because they have special handling for constructing ordinary (albeit private) properties,
-// and also for anonymous types, but they have no support for the standard constructor-based init idiom
-// that records use.
-//    public record RTheorem<T1, T2>(T1 X, T2 Y);
-// But if the record type offers a default ctor, it's happy.
+// This doesn't work because the code that constructs an output representing the solution
+// does not have the necessary special handling for the standard constructor-based initialization
+// idiom that records use. (Anonymous types and ordinary properties work only because there
+// is special handling for them. Special handling could be added for this too but we don't
+// currently have it
 public record RecordTheorem<T1, T2>
 {
     public T1 X { get; init; } = default!;
